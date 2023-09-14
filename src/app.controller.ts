b22cac344b3f2387014d8ff9fc210e748a38a5a2
LookLikeLIe <quilimonjaro@gmail.com>
2023-09-14 20:25:14 +0300

    3nd Lab

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,20 @@
-import { Get, Controller, Render, UseInterceptors } from "@nestjs/common";
+import { Get, Controller, Render, UseInterceptors, Param,
+  Post,
+  Body,
+  Put,
+  Delete,} from "@nestjs/common";
 import { AppInterceptor } from "./app.interceptor";
+import { UserService } from './user.service';
+import { PostService } from './post.service';
+import { User as UserModel, Product as ProductModel, Order as OrderModel,  Review as ReviewModel} from '@prisma/client';
 
 @Controller()
 @UseInterceptors(AppInterceptor)
 export class AppController {
+  constructor(
+      private readonly userService: UserService,
+      private readonly postService: PostService,
+  ) {}
   @Get()
   @Render('pages/index')
   root() {
