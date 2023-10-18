a6c4c7b247f6baf676e523801b171ccf269fad1c
LookLikeLIe <quilimonjaro@gmail.com>
2023-10-18 14:18:34 +0300

    6 lab test signup

---
+++ public/assets/scripts/registration.js
@@ -0,0 +1,75 @@
+let login = document.querySelector("#login");
+let pass = document.querySelector("#pass");
+let mail = document.querySelector("#mail");
+let name = document.querySelector("#name");
+
+const createUser = (data) => {
+    fetch('/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        mode: 'same-origin',
+        body: JSON.stringify(data),
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return error();
+            }
+        })
+        .then((response) => {
+            setServerTime(response);
+            alert('You are successfully registered in system');
+            window.location.href = '/';
+        });
+};
+
+const getSignUpData = () => {
+    return {
+        name: name,
+        email: mail,
+        login: login,
+        password: pass,
+        superToken: '',
+    };
+};
+
+
+let btn = document.querySelector("#add-btn");
+btn.addEventListener("click", () => {
+    let data = getSignUpData();
+
+    fetch('/auth/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        mode: 'same-origin',
+        body: JSON.stringify({
+            formFields: [
+                {
+                    id: 'email',
+                    value: data.email,
+                },
+                {
+                    id: 'password',
+                    value: data.password,
+                },
+            ],
+        }),
+    }).then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(`Failed to sign un user: ${response.statusText}`);
+        }
+    }).then((response) => {
+        data.superTokenId = response.user.id;
+        createUser(data);
+        window.location.href = '/';
+    })
+});
